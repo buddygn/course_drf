6ec4b77e8db95eb7652c2bbd2774983ec9a2586e
buddygn <buddygn@gmail.com>
2022-02-24 23:46:16 -0300

    Criação serializers.py

---
+++ cursos/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+
+from .models import Avaliacao, Curso
+
+
+class AvaliacaoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Avaliacao
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+        fields = {
+            'id',
+            'curso',
+            'email',
+            'comentario',
+            'avaliacao',
+            'criacao',
+            'ativo',
+        }
+
+
+class CursoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Curso
+        fields = {
+            'id',
+            'titulo',
+            'url',
+            'criacao',
+            'ativo',
+        }
---
+++ cursos/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from cursos import views
+
+urlpatterns = [
+    # path('/', views.),
+]
