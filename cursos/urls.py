d9b5278b5476ee13b388bea9aba0e059ce078606
buddygn <buddygn@gmail.com>
2022-03-07 23:53:34 -0300

    Use generic views with DRF

--- cursos/urls.py
+++ cursos/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 from cursos import views
 
 urlpatterns = [
-    path('cursos/', views.CursoApiView.as_view(), name='cursos'),
-    path('avaliacoes/', views.AvaliacaoApiView.as_view(), name='avaliacoes'),
+    path('cursos/', views.CursosAPIView.as_view(), name='cursos'),
+    path('cursos/<int:pk>/', views.CursoAPIView.as_view(), name='curso'),
+    path('cursos/<int:curso_pk>/avaliacoes/', views.AvaliacoesAPIView.as_view(), name='curso_avaliacoes'),
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>/', views.AvaliacaoAPIView.as_view(),
+         name='curso_avaliacao'),
+
+    path('avaliacoes/', views.AvaliacoesAPIView.as_view(), name='avaliacoes'),
+    path('avaliacoes/<int:avaliacao_pk>/', views.AvaliacaoAPIView.as_view(), name='avaliacoes'),
 ]
